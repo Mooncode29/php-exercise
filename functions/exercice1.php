f7869602aa265b85313f13db5ed30865dae07352
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-09 16:07:01 +0100

    exercice 1 ok : creation d'une fonction qui retourne true.

---
+++ functions/exercice1.php
@@ -0,0 +1,10 @@
+<h2>Exercice 1</h2>
+<?php
+function afficheTrue (){
+	return true;
+
+}
+ echo afficheTrue();
+ var_dump(afficheTrue());
+
+?>
---
+++ functions/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<?php
+		include "exercice1.php";
+	?>
+</body>
+</html>
