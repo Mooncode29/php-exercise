49dbbdef4d75469e2472fe5abcad338b2547378c
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-09 10:38:13 +0100

    exercice2 ok : création deux variables, multiplication des deux variables, affichage et incrémentation

--- loop/exercice1.php
+++ loop/exercice1.php
@@ -1,7 +1,7 @@
 <?php
 $i = 0;
 while ($i <= 10):
-	echo $i;
+	echo $i.' ';
 	$i++;
 
 endwhile;
---
+++ loop/exercice2.php
@@ -0,0 +1,11 @@
+<?php
+$number1 = 0;
+$number2 = 10;
+//saut-de ligne "\n"
+for (;$number1<=20; $number1++):
+	$multiply = $number1*$number2;
+	echo $multiply."\n";
+
+endfor;
+
+?>
