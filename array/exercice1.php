709e3b75d67db0aa337c8d81fadfacddee5ee506
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-10 11:31:14 +0100

    création dossier array, exercice 1 ok : initialisation d'un tableau avec les mois

---
+++ array/exercice1.php
@@ -0,0 +1,18 @@
+<h2>Exercice 1</h2>
+<?php
+	$mois = [
+		'janvier',
+		'février',
+		'mars',
+		'avril',
+		'mai' ,
+		'juin',
+		'juillet',
+		'aout',
+		'septembre',
+		'octobre',
+		'novembre',
+		'décembre'
+	];
+var_dump($mois);
+?>
---
+++ array/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Array</title>
+</head>
+<body>
+	<h1>Tableaux</h1>
+	<?php
+		include 'exercice1.php';
+	?>
+
+</body>
+</html>
