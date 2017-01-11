8c77b4c1d3da502f8738accf9970a8c80343d990
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-11 13:42:56 +0100

    exercice 5 ok : creation formulaire qui renvoie sur index.php avec la methode get

---
+++ form/exercice5/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Formulaire exo 5</title>
+</head>
+<body>
+<form action="" method="GET">
+	<select name="genre">
+		<option value="Mme">Mme</option>
+		<option value="M.">M.</option>				
+	</select>
+	<div>
+		<label for="name">Nom</label>
+		<input type="text" name="name">
+	</div>
+	<div>
+		<label for="prenom">Prénom</label>
+		<input type="text" name="prenom">
+	</div>
+	<button type="submit" name="envoyer">Envoyer</button>
+</form>
+	
+</body>
+</html>
