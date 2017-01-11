270bd513b3ccacf06854eea79572d4431772df05
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-11 11:56:29 +0100

    exo 2 ok : creation formulaire avec methode post

---
+++ form/exercice2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>form exercice 2</title>
+</head>
+<body>
+<form action="../user.php" method="POST">
+	<div>
+		<label for="name">Nom</label>
+		<input type="text" name="name">
+	</div>
+	<div>
+		<label for="prenom">Prénom</label>
+		<input type="text" name="prenom">
+	</div>
+<button>Envoyer</button>
+</form>
+	
+</body>
+</html>
--- form/user.php
+++ form/user.php
@@ -1,3 +1,5 @@
 <?php
 
 echo $_GET['name']." ".$_GET['prenom'];
+
+echo $_POST['name']." ".$_POST['prenom'];
