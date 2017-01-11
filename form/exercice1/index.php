faf2fc18a820c0498248688220178c7cfeb533b4
Mooncode29 <irene.ravelonarivo@gmail.com>
2017-01-11 10:34:11 +0100

    exo 1 : ok, creation formulaire avec methode get

---
+++ form/exercice1/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Formulaire</title>
+</head>
+<body>
+<form action="../user.php" method="GET">
+	<div>
+		<label for="name">Nom</label>
+		<input type="text" name="name">
+	</div>
+	<div>
+		<label for="prenom">Prénom</label>
+		<input type="text" name="prenom">
+	</div>
+<button>Envoyer</button>
+	
+</form>
+</body>
+</html>
